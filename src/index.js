ac1fcfa6926b8ef08d41cd9d4d7d6c1b7d435748
Vladimir Bondar <vladi.bondar@gmail.com>
2021-09-22 12:30:50 +0300

    fix logo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'modern-normalize/modern-normalize.css';
 import 'notiflix/dist/notiflix-aio-3.0.2.min.js';
 
-import './images/pixabay-logo.svg';
+import './images/sprite.svg';
 import './sass/main.scss';
 import './js/image-finder';
