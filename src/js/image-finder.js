d450d5dccafb67ac50108996d1feb7d31953ca2f
Vladimir Bondar <vladi.bondar@gmail.com>
2021-09-23 01:12:47 +0300

    add smooth scroll

--- src/js/image-finder.js
+++ src/js/image-finder.js
@@ -1,12 +1,14 @@
-import { Notify, Report, Confirm, Loading, Block } from 'notiflix';
+import { Notify, Loading } from 'notiflix';
 import imagesTemplate from '../templates/image-cards.hbs';
 import refs from './refs';
 import PixabayApiService from './apiService';
+import LoadMoreBtn from './components/load-more-btn';
 
 const pixabay = new PixabayApiService();
+const loadMoreBtn = new LoadMoreBtn('.js-load-more__btn');
 
 refs.searchForm.onsubmit = onSearch;
-refs.loadMoreBtn.onclick = addImages;
+loadMoreBtn.refs.button.onclick = addImages;
 
 function onSearch(e) {
   e.preventDefault();
@@ -25,6 +27,7 @@ function onSearch(e) {
 
 async function addImages() {
   Loading.circle();
+  loadMoreBtn.hide();
 
   try {
     const { hits, totalHits } = await pixabay.fetchImages();
@@ -32,12 +35,22 @@ async function addImages() {
     if (!totalHits)
       throw 'Sorry, there are no images matching your search query. Please try again.';
 
-    if (pixabay.page === 2) Notify.success(`Hooray! We found ${totalHits} images.`);
-
-    if (pixabay.perPage * pixabay.page >= totalHits)
-      Notify.info("We're sorry, but you've reached the end of search results.");
-
     appendImagesMarkup(hits);
+    loadMoreBtn.show();
+
+    if (pixabay.page === 2) {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+    } else {
+      const { height } = refs.galleryContainer.firstElementChild.getBoundingClientRect();
+
+      window.scrollBy({
+        top: height * 2,
+        behavior: 'smooth',
+      });
+
+      if (pixabay.perPage * pixabay.page >= totalHits)
+        Notify.info("We're sorry, but you've reached the end of search results.");
+    }
   } catch (error) {
     Notify.failure(`${error}`);
   } finally {
