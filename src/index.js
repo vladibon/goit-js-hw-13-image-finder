08e0653897f893a6488a6245f50f315156ddae55
Vladimir Bondar <vladi.bondar@gmail.com>
2021-09-23 01:09:47 +0300

    add load-more-button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import 'modern-normalize/modern-normalize.css';
 import 'notiflix/dist/notiflix-aio-3.0.2.min.js';
 
-import './images/sprite.svg';
-import './sass/main.scss';
+import './js/components/notiflix-options';
+import './js/components/load-more-btn';
 import './js/image-finder';
+
+import './sass/main.scss';
+import './images/sprite.svg';
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
 export default {
   searchForm: document.querySelector('.js-search-form'),
   galleryContainer: document.querySelector('.js-gallery'),
-  loadMoreBtn: document.querySelector('.js-load-more__btn'),
+  // loadMoreBtn: document.querySelector('.js-load-more__btn'),
 };
