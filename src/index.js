540683ce35675c400758041cf659eb051f8d509a
Vladimir Bondar <vladi.bondar@gmail.com>
2021-09-23 15:25:00 +0300

    add theme-switch

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import 'modern-normalize/modern-normalize.css';
 import 'notiflix/dist/notiflix-aio-3.0.2.min.js';
 
-import './js/components/notiflix-options';
-import './js/components/load-more-btn';
+import './js/notiflix-options';
 import './js/image-finder';
+import './js/theme-switch';
 
 import './sass/main.scss';
 import './images/sprite.svg';
