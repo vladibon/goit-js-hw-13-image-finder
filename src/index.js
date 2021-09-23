aeac677d3cebefdb098a1d62541cf0d6915762ca
Vladimir Bondar <vladi.bondar@gmail.com>
2021-09-23 23:30:58 +0300

    rename files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import 'modern-normalize/modern-normalize.css';
 import 'notiflix/dist/notiflix-aio-3.0.2.min.js';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import './js/notiflix-options';
-import './js/image-finder';
-import './js/theme-switch';
+import './js/image-loading';
+import './js/components/theme-switch';
 
 import './sass/main.scss';
 import './images/sprite.svg';
