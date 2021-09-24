4778f7c0db889384228563a0896c89ab7196fc27
Vladimir Bondar <vladi.bondar@gmail.com>
2021-09-24 22:55:54 +0300

    transfer notiflix-options to components dir

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import 'modern-normalize/modern-normalize.css';
 import 'notiflix/dist/notiflix-aio-3.0.2.min.js';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import './js/notiflix-options';
 import './js/image-loading';
+import './js/components/notiflix-options';
 import './js/components/theme-switch';
 
 import './sass/main.scss';
